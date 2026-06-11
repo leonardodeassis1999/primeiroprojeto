3c9f3329981d75896d0db3e3bca7c169f5f430e6
thalesxzzl <to1658524@gmail.com>
2026-06-10 21:36:50 -0300

    atv.poke

--- pokemons/poke.py
+++ pokemons/poke.py
@@ -1,6 +1,8 @@
 import requests
 
-url = "https://pokeapi.co/api/v2/pokemon/pikachu"
+nome = input("Qual é o pokemon?").lower().lstrip()
+
+url = "https://pokeapi.co/api/v2/pokemon/" + nome 
 
 resposta = requests.get(url)
 
@@ -8,7 +10,18 @@ print("status: ", resposta.status_code)
 
 dados = resposta.json()
 
+print("--- Informações gerais---")
 print("Nome: ", dados['name'])
 print("Número: ", dados['id'])
 print("Altura: ", dados['height'])
 print("Peso: ", dados['weight'])
+
+print ("--- Habilidades ---")
+for item in dados["types"]:
+    print("-", item["type"]["name"])
+
+print ("--- Stats ---")
+for stat in dados["stats"]:
+    print ("-", stat["stat"]["name"],":",stat["base_stat"])
+
+#atividade fazer função para monstrar e exibir

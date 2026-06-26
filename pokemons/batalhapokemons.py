ec364df37bcc2b5a477175d5ec11009a3ff8d0e1
thalesxzzl <to1658524@gmail.com>
2026-06-25 21:56:54 -0300

    pokeAtualizado

--- pokemons/batalhapokemons.py
+++ pokemons/batalhapokemons.py
@@ -1,39 +1,5 @@
 import requests
 
-def pokemon_listar():
-
-    quantidade = input("Quantos pokemons deseja listar?").strip().lower() 
-    pokemons_disponiveis = "https://pokeapi.co/api/v2/pokemon?limit="+quantidade
-    respostapokemons = requests.get(pokemons_disponiveis)
-    dadospokemons_disponiveis = respostapokemons.json()
-
-    for pokemon in dadospokemons_disponiveis["results"]:
-        print("-",pokemon["name"])
-
-def pokemon_buscar():
-
-    nome = input("Qual é o pokemon?").lower().lstrip()
-
-    url = "https://pokeapi.co/api/v2/pokemon/" + nome 
-
-    resposta = requests.get(url)
-    dados = resposta.json()
-
-    print("status: ", resposta.status_code)
-    print("--- Informações gerais---")
-    print("Nome: ", dados['name'])
-    print("Número: ", dados['id'])
-    print("Altura: ", dados['height'])
-    print("Peso: ", dados['weight'])
-
-    print ("--- Habilidades ---")
-    for item in dados["types"]:
-        ("-", item["type"]["name"])
-
-        print ("--- Stats ---")
-    for stat in dados["stats"]:
-        print ("-", stat["stat"]["name"],":",stat["base_stat"])
-
 def Batalha():
 
     def pokemon01():
@@ -89,28 +55,4 @@ def Batalha():
         print("")
         print("Pokemon01 ganhou!")
 
-def menu():
-    
-    while True:
-        print("\n--- MENU POKÉMON ---")
-        print("1 - Listar Pokémons")
-        print("2 - Buscar Pokémon")
-        print("3 - Batalha")
-        print("0 - Sair")
-
-        opcao = input("Escolha uma opção: ").strip()
-
-        if opcao == "1":
-            pokemon_listar()
-        elif opcao == "2":
-            pokemon_buscar()
-        elif opcao == "3":
-            Batalha()
-        elif opcao == "0":
-            print("Saindo... Até mais!")
-            break
-        else:
-            print("Opção inválida! Tente novamente.")
-
-menu()
-#atividade fazer função para monstrar e exibir
+Batalha()
